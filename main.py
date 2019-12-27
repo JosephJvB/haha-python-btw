34749f5cdb4f90f0445e53925645ef935da49e0e
JosephJvB <joevanbo@pm.me>
2019-12-28 10:51:39 +1300

    scuffed time loop

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import os
 import requests
+import time
 from client import Client
 
 if __name__ == '__main__':
-    c = Client()
-    c.run()
+    timeout = 60 * 60 * 24
+    while True:
+        if os.getenv('status') == 'live':
+            c = Client()
+            c.run()
+        time.sleep(timeout)

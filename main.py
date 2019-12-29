71540d4a8afa6cdbdcf6f1fbbd833f09ba189bf9
JosephJvB <joevanbo@pm.me>
2019-12-29 13:20:05 +1300

    im dumbim dumb

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from client import Client
 if __name__ == '__main__':
     timeout = 60 * 60 * 24
     while True:
-        print(os.getenv('status'), '???')
         if os.getenv('status') == 'live':
             c = Client()
             c.run()
-        print('SCHLEEP TILL', datetime.now() + timedelta(days=1), '\n\n')
+        elif os.getenv('status') == 'alter':
+            timeout = timeout - (60 * 60 * 4)
         time.sleep(timeout)

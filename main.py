b66138d29398e589cfc75620c3904fee4af30288
JosephJvB <joevanbo@pm.me>
2019-12-29 13:44:40 +1300

    test

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import os
 import requests
 import time
-from datetime import datetime, timedelta
+from datetime import datetime
 from client import Client
 
 if __name__ == '__main__':
     timeout = 60 * 60 * 24
     while True:
+        print('asdasdadssd', datetime.now().hour)
         if os.getenv('status') == 'live':
             c = Client()
             c.run()
-        elif os.getenv('status') == 'alter':
-            timeout = timeout - (60 * 60 * 4)
         time.sleep(timeout)

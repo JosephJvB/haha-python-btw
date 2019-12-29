4edabd8ad98776f33b3ebf8dedc75116ba5e47a1
JosephJvB <joevanbo@pm.me>
2019-12-29 13:57:10 +1300

    i htink it has to be this

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ from datetime import datetime
 from client import Client
 
 if __name__ == '__main__':
-    timeout = 60 * 58
+    timeout = 60 * 60
     while True:
         if datetime.now().hour == int(os.getenv('hour')):
             c = Client()
             c.run()
+        else:
+            print('not yet :)')
         time.sleep(timeout)

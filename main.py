2a1d9fcef503aedbd5950e081e4bd2f47dc59839
JosephJvB <joevanbo@pm.me>
2019-12-29 13:13:35 +1300

    what?

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from client import Client
 if __name__ == '__main__':
     timeout = 60 * 60 * 24
     while True:
+        print(os.getenv('status'), '???')
         if os.getenv('status') == 'live':
             c = Client()
             c.run()
